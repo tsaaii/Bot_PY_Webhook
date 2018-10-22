2f80f41245ccc4e6808dc560b81e216a52b94587
tsaaii <stsurapa@uwaterloo.ca>
2018-10-21 23:55:10 -0400

    first action added

--- rapid2.py
+++ rapid2.py
@@ -76,3 +76,45 @@ table_dept_Cosmetics=new18[['POG Category','Department']]
 
 #tables look good. each and every department has its own tables
 #tables look good
+
+app = Flask(__name__)
+@app.route('/webhook', methods=['POST'])
+
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print("Request:")
+    print(json.dumps(req, indent=4))
+    res = makeWebhookResult(req)
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("")
+    
+    if req.get("result").get("action") == "sup":
+       
+        speech = 'Hi'
+        print("Response:", speech)
+
+        return {
+            "speech": modified,
+            "displayText": modified,
+            "data": {},
+            "contextOut": [],
+            
+        }
+    else:
+        return{"displayText": "I dont know what you are talking about"}
+
+if __name__ == '__main__':
+    os.putenv('FLASK_ENV', 'development')
+    port = int(os.getenv('PORT', 5000))
+
+    print ("Starting app on port %d" %(port))
+
+    app.run(debug=True, port=port, host='0.0.0.0')
